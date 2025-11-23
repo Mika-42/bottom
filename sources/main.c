39ef3cd5ab4dae74166ddfcd5fb54dfbf19467c2
ieto <ietob8@gmail.com>
2025-11-23 18:59:39 +0100

    modif state

--- sources/main.c
+++ sources/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *arg[])
-{
+{	
+	program_state_t *state = command_parse_options(argc, argv);
+	if (state == NULL) {
+	   // ya une erreur 
+	   return EXIT_FAILURE
+ 	}
+ 	if (state->help) {
+ 	//si ya loption help dmd (à rajouter laide)
+ 	manager_free_state(state);
+ 	return EXIT_SUCCESS;
+ 	
 	printf("Hello World!\n");
 	return 0;
 }		
