a8594989aedf4a1b477552dbb841fd6517cb96cb
ieto <ietob8@gmail.com>
2025-11-23 19:06:38 +0100

    minor changes

--- sources/main.c
+++ sources/main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "command_option.h"
 
-int main(int argc, char *arg[])
+int main(int argc, char *argv[])
 {	
-	program_state_t *state = command_parse_options(argc, argv);
-	if (state == NULL) {
-	   // ya une erreur 
-	   return EXIT_FAILURE
- 	}
- 	if (state->help) {
- 	//si ya loption help dmd (à rajouter laide)
- 	manager_free_state(state);
- 	return EXIT_SUCCESS;
- 	
-	printf("Hello World!\n");
-	return 0;
-}		
+    program_state_t *state = command_parse_options(argc, argv);
+    if (state == NULL) {
+        // ya une erreur 
+        return EXIT_FAILURE;
+    }
+
+    if (state->help) {
+        // si l'option --help est demandée
+        print_help();
+        manager_free_state(state);
+        return EXIT_SUCCESS;
+    }
+
+
+    // libération de la mémoire
+    manager_free_state(state);
+
+    return EXIT_SUCCESS;
+}
+
