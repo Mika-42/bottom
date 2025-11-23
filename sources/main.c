2a7b4c628671dc871ff0295cda442c5736bd6e86
ieto <ietob8@gmail.com>
2025-11-23 18:07:45 +0100

    juste un test

--- sources/main.c
+++ sources/main.c
@@ -1,5 +1,8 @@
-#include "command_option.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]){
-	return command_run(argc, argv);
-}
+int main(int argc, char *arg[])
+{
+	printf("Hello World!\n");
+	return 0;
+}		
