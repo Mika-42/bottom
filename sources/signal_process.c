c2fb1488c5c3875dd538020c3e9dd3375ffb0c43
Matis <matis.mikael@gmail.com>
2025-11-29 19:13:14 +0100

    fix errors

--- sources/signal_process.c
+++ sources/signal_process.c
@@ -1,9 +1,6 @@
 
 #include "signal_process.h"
 
-<<<<<<< HEAD
-void kill_process(Processus, p){}
-=======
 void kill_process(Processus p);
 
 void stop_process(Processus p);
@@ -11,5 +8,3 @@ void stop_process(Processus p);
 void term_process(Processus p);
 
 void reboot_process(Processus p);
->>>>>>> 0174d8225e71274f42449c3b1ce19b4aca2b3539
-
