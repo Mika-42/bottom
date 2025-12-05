6a188af3d3214482404863808381a5d4125ae40c
Matis <matis.mikael@gmail.com>
2025-12-05 21:12:33 +0100

    add remove_if function

--- sources/processus/processus_array.c
+++ sources/processus/processus_array.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-processus_t *proc_array_get_last(processus_array_t *array)
-{
+processus_t *proc_array_get_last(processus_array_t *array) {
 	return (!array || array->size == 0 || !array->data) ? 
 		nullptr : &array->data[array->size - 1];
 }
@@ -22,6 +21,19 @@ processus_t *proc_array_emplace_back(processus_array_t *array) {
 	array->size = new_size;
 	
 	return proc_array_get_last(array);	
+}
+
+void remove_if(processus_array_t *array, bool(*predicate)(processus_t*)) {
+	
+	size_t write_index = 0;
+
+	for(size_t i = 0; i < array->size; ++i) {
+		processus_t *current = &(array->data[i]);
+		
+		///> Ici on écrase les valeurs à retirer avec les valeurs que l'on souhaite garder
+		if(!predicate(current)) array->data[write_index++] = *current;
+	}
+	array->size = write_index;
 }
 
 void proc_array_sort(processus_array_t *array, proc_compare_t cmp) {
