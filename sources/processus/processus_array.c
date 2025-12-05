4179d50ce62ad7f4ac1226fd541eb99fb6e5d58b
Matis <matis.mikael@gmail.com>
2025-12-05 20:13:10 +0100

    add sort function for processus

--- sources/processus/processus_array.c
+++ sources/processus/processus_array.c
@@ -1,6 +1,74 @@
 #include "processus_array.h"
+#include <stdlib.h>
+#include <string.h>
 
-
-void proc_array_push(processus_array_t *array, const processus_t *proc) {
-	
+processus_t *proc_array_get_last(processus_array_t *array)
+{
+	return (!array || array->size == 0 || !array->data) ? 
+		nullptr : &array->data[array->size - 1];
 }
+
+processus_t *proc_array_emplace_back(processus_array_t *array) {
+
+	if(!array) return nullptr;
+
+	const size_t new_size = array->size + 1;
+
+	processus_t *temp = realloc(array->data, new_size * sizeof(*temp));
+
+	if(!temp) return nullptr;
+	
+	array->data = temp;
+	array->size = new_size;
+	
+	return proc_array_get_last(array);	
+}
+
+void proc_array_sort(processus_array_t *array, proc_compare_t cmp) {
+
+	if (!array || !array->data || !cmp) return;
+	
+	qsort(array->data, array->size, sizeof(processus_t),
+        (int (*)(const void*, const void*))cmp);
+}
+
+
+int pid_asc(const processus_t *lhs, const processus_t *rhs) {
+	return lhs->pid - rhs->pid;
+}
+
+int pid_dsc(const processus_t *lhs, const processus_t *rhs) {
+	return rhs->pid - lhs->pid;
+}
+
+int state_asc(const processus_t *lhs, const processus_t *rhs) {
+	return lhs->state - rhs->state;
+}
+
+int state_dsc(const processus_t *lhs, const processus_t *rhs) {
+	return rhs->state - lhs->state;
+}
+
+int rss_asc(const processus_t *lhs, const processus_t *rhs) {
+	return lhs->ram_rss - rhs->ram_rss;
+}
+
+int rss_dsc(const processus_t *lhs, const processus_t *rhs) {
+	return rhs->ram_rss - lhs->ram_rss;
+}
+
+int name_asc(const processus_t *lhs, const processus_t *rhs) {
+	return strcmp(lhs->name, rhs->name);
+}
+
+int name_dsc(const processus_t *lhs, const processus_t *rhs) {
+        return strcmp(rhs->name, lhs->name);
+}
+int user_asc(const processus_t *lhs, const processus_t *rhs) {
+        return strcmp(lhs->user, rhs->user);
+}
+int user_dsc(const processus_t *lhs, const processus_t *rhs) {
+        return strcmp(rhs->user, lhs->user);
+}
+
+
--- sources/processus/processus_array.h
+++ sources/processus/processus_array.h
@@ -8,6 +8,21 @@ typedef struct processus_array_t {
 	size_t		size;
 } processus_array_t;
 
-void proc_array_push(processus_array_t *array, const processus_t *proc);
+typedef int (*proc_compare_t)(const processus_t *, const processus_t *);
+
+processus_t *proc_array_get_last(processus_array_t *array);
+processus_t *proc_array_emplace_back(processus_array_t *array);
+void proc_array_sort(processus_array_t *array, proc_compare_t cmp);
+
+int pid_asc(const processus_t *lhs, const processus_t *rhs);
+int pid_dsc(const processus_t *lhs, const processus_t *rhs);
+int state_asc(const processus_t *lhs, const processus_t *rhs);
+int state_dsc(const processus_t *lhs, const processus_t *rhs);
+int rss_asc(const processus_t *lhs, const processus_t *rhs);
+int rss_dsc(const processus_t *lhs, const processus_t *rhs);
+int name_asc(const processus_t *lhs, const processus_t *rhs);
+int name_dsc(const processus_t *lhs, const processus_t *rhs);
+int user_asc(const processus_t *lhs, const processus_t *rhs);
+int user_dsc(const processus_t *lhs, const processus_t *rhs);
 
 #endif //PROCESSUS_ARRAY_H
