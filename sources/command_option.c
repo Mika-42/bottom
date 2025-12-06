0911c846562b1875096e76350d37e611046dbd99
ieto <ietob8@gmail.com>
2025-12-06 17:26:20 +0100

    changement

--- sources/command_option.c
+++ sources/command_option.c
@@ -6,15 +6,9 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <string.h>
-#include <unistd.h>
-#include <errno.h>
-
-static void print_help();
-static int handle_login_option(options_prog *options);
-static int check_config_file_permissions(options_prog *options);
-
 
 static void print_help(){
+
 	const char* help_message = "-h ou --help : affiche l'aide du programme ainsi que la syntaxe d'exécution du programme\n"
    	 "--dry-run : test l'accès à la liste des processus sur la machine locale et/ou distante sans les afficher\n"
    	 "-c ou --remote-config : spécifie le chemin vers le fichier de configuration contenant les informations de connexion sur les machines distantes\n"
@@ -32,56 +26,6 @@ static void print_help(){
 	printf("%s", help_message);
 }
 
-static int handle_login_option(options_prog *options) {
-    if (options->login != NULL) {
-        char *separator = strchr(options->login, '@');
-        
-        if (separator == NULL) {
-            fprintf(stderr, "Erreur: Le format de l'option -l doit etre 'username@remote_server'.\n");
-            return -1;
-        }
-
-        options->remote_server = strdup(separator + 1);
-        if (options->remote_server == NULL) {
-            perror("Erreur d'allocation memoire (remote_server)");
-            return -1;
-        }
-
-        size_t username_len = separator - options->login;
-        options->username = (char *)malloc(username_len + 1);
-        if (options->username == NULL) {
-            perror("Erreur d'allocation memoire (username)");
-            free(options->remote_server);
-            options->remote_server = NULL;
-            return -1;
-        }
-
-        strncpy(options->username, options->login, username_len);
-        options->username[username_len] = '\0';
-    }
-    return 0;
-}
-
-static int check_config_file_permissions(options_prog *options) {
-    if (options->remote_config != NULL) {
-        struct stat file_stat;
-        
-        if (stat(options->remote_config, &file_stat) == -1) {
-            fprintf(stderr, "Erreur: Impossible d'acceder au fichier de configuration '%s'. %s\n", 
-                    options->remote_config, strerror(errno));
-            return -1;
-        }
-
-        if ((file_stat.st_mode & 0777) != 0600) {
-            fprintf(stderr, "Erreur: Le fichier de configuration '%s' doit avoir les permissions 600 (rw-------).\n", 
-                    options->remote_config);
-            return -1;
-        }
-    }
-    return 0;
-}
-
-
 int command_run(int argc, char *argv[], options_prog *options){
 	int opt = 0;
 	struct option long_opts[] = {
@@ -97,9 +41,6 @@ int command_run(int argc, char *argv[], options_prog *options){
 		{.name="all",.has_arg=0,.flag=0,.val='a'},
 		{.name=0,.has_arg=0,.flag=0,.val=0},
 	};
-    
-    optind = 1; 
-
 	while((opt = getopt_long(argc, argv, "hc:t:P:l:s:u:p:a", long_opts, NULL)) != -1) {
 		switch (opt) {
 			case 'h':
@@ -121,7 +62,7 @@ int command_run(int argc, char *argv[], options_prog *options){
 				break;
 
 			case 'P':
-				options->port = atoi(optarg); 
+				options->port = atoi(optarg);
 				break;
 
 			case 'l':
@@ -144,16 +85,44 @@ int command_run(int argc, char *argv[], options_prog *options){
 				options->all = true;
 				break;
 			case '?':
-				return -1;
+				exit(-1);
 				break;
+
 		}
 	}
-    
-    if (handle_login_option(options) != 0) {
-        return -1;
-    }
 
-	if (options->port == 0 && options->connexion_type != NULL) {
+	if (options->login != NULL) {
+        char *separator = strchr(options->login, '@');
+        size_t username_len; 
+
+        if (separator == NULL) {
+            fprintf(stderr, "Erreur: Le format de l'option -l doit etre 'username@remote_server'.\n");
+            return -1;
+        }
+
+        options->remote_server = strdup(separator + 1);
+        if (options->remote_server == NULL) {
+            perror("Erreur d'allocation memoire (remote_server)");
+            return -1;
+        }
+
+        username_len = separator - options->login;
+        options->username = (char *)malloc(username_len + 1);
+        if (options->username == NULL) {
+            perror("Erreur d'allocation memoire (username)");
+            free(options->remote_server); 
+            options->remote_server = NULL;
+            return -1;
+        }
+
+        strncpy(options->username, options->login, username_len);
+        options->username[username_len] = '\0';
+    }
+	
+    if (verif_login(options) != 0) {
+        return -1;
+
+	if (options->port == 0)    {
 		if (strcmp(options->connexion_type, "ssh") == 0){
 			options->port = 22;
 		}
@@ -163,25 +132,35 @@ int command_run(int argc, char *argv[], options_prog *options){
 	}
 
 	if (options->remote_config == NULL && options->remote_server == NULL) {
-		options->all = false;
+		options->all=false;
 	}
 
 	if (options->remote_config == NULL) {
-		DIR *d = opendir(".");
-        if (d != NULL) {
+        DIR *d = opendir(".");
+        if (d != NULL) { 
             for (struct dirent *element; (element = readdir(d)) != NULL; ) {
                 if (strcmp(element->d_name, ".config") == 0) {
-                    options->remote_config = ".config"; 
-                    break;
-                }
+                    options->remote_config = ".config";
+                    break; 
+				}
             }
             closedir(d);
-        }
+		}
 	}
-    
-    if (check_config_file_permissions(options) != 0) {
-        return -1;
-    }
-	
+	struct stat fichier;
+	if (options->remote_config != NULL) {
+		if ((fichier.st_mode & 0777) != 0600) {
+			printf("Le fichier de configuration doit avoir les permissions 600 ( rw-------)\n");
+			exit(1);
+		}
+	}
+
+	if (options->remote_config == NULL && options->remote_server == NULL) {
+        options->all=false;
+	}
+
 	return 0;
+
 }
+
+
