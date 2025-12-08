eeb6adcaecb498b877ed6bc53b7a0d757d87c7d6
ieto <ietob8@gmail.com>
2025-12-08 23:03:27 +0100

    max length char

--- sources/command_option.h
+++ sources/command_option.h
@@ -7,25 +7,25 @@ typedef struct
 {
     bool help;
     bool dry_run;
-    char *remote_config[MAX_LENGTH];    
-    char *connexion_type[MAX_LENGTH];
+    char remote_config[MAX_LENGTH];    
+    char connexion_type[MAX_LENGTH];
     int port;
-    char *login[MAX_LENGTH]; 
-    char *remote_server[MAX_LENGTH]; 
-    char *username[MAX_LENGTH];
-    char *password[MAX_LENGTH]; 
+    char login[MAX_LENGTH]; 
+    char remote_server[MAX_LENGTH]; 
+    char username[MAX_LENGTH];
+    char password[MAX_LENGTH]; 
     bool all;
 
 } options_prog; 
 
 typedef struct 
 {
-    char *name_server[MAX_LENGTH];
-    char *adress_server[MAX_LENGTH];
+    char name_server[MAX_LENGTH];
+    char adress_server[MAX_LENGTH];
     int port; 
-    char *username[MAX_LENGTH];
-    char *password[MAX_LENGTH]; 
-    char *type_co[7];
+    char username[MAX_LENGTH];
+    char password[MAX_LENGTH]; 
+    char type_co[7];
 } fichier;
 
 int command_run(int argc, char *argv[],options_prog *options);
