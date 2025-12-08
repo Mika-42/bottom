3f04df072fdce90566d131a68d207c0b0b4c92ce
potdfleure <franjolauh@gmail.com>
2025-12-08 18:47:08 +0100

    creation of ssh_connexion.c/.h and basic functions

---
+++ sources/ssh_connexion.h
@@ -0,0 +1,12 @@
+#ifndef SSH_CONNEXION
+#define SSH_CONNEXION
+
+#include <libssh/libssh.h>
+
+void ssh_end_session(ssh_session session);
+
+ssh_session ssh_connexion_init(char *host, int port, char *user, char *password);
+
+int ssh_dry_run(ssh_session session); //EXIT_SUCCESS si réussi sinon EXIT_FAILURE
+
+#endif //SSH_CONNEXION
