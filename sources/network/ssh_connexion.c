93c6f21ba0b8fc5e652ec340179fea7c5bcf643a
potdfleure <franjolauh@gmail.com>
2025-12-16 11:36:40 +0100

    adding function ssh_get_file

--- sources/network/ssh_connexion.c
+++ sources/network/ssh_connexion.c
@@ -31,7 +31,7 @@ ssh_session ssh_connexion_init(char *host, int port, char *user, char *password)
 
 int ssh_cmd_exec(ssh_session session, char *cmd, char *output, size_t len_max) {
 	ssh_channel channel = ssh_channel_new(session);
-	if (channel == nullptr) return EXIT_FAILURE;
+	if (channel == nullptr) return SSH_ERROR;
 	if (ssh_channel_open_session(channel) != SSH_OK) {
 		ssh_channel_free(channel);
 		return EXIT_FAILURE;
@@ -80,4 +80,59 @@ int ssh_cont_processus(ssh_session session, int pid) {
 	char cmd[64];
 	snprintf(cmd, sizeof(cmd), "kill -CONT %d", pid);
 	return ssh_cmd_exec(session, cmd, NULL, 0);
+}
+
+/**
+ * @brief Met le contenue du fichier /proc/file dans le buffer
+ *
+ * @param session Session ssh
+ * @param buffer Adresse de la chaine de caractère dans laquel sera écrit le contenue du fichier
+ * @param file Fichier du dossier /proc/ qui sera lu
+ */
+int ssh_get_file(ssh_session session, char **buffer, char *file) {
+	int n;
+	char *buf = nullptr;
+	size_t size = 0, capacity = 8192;
+
+	ssh_channel channel = ssh_channel_new(session);
+	if (!channel) return SSH_ERROR;
+	
+	if (ssh_channel_open_session(channel) != SSH_OK) goto error;
+
+	char cmd[128];
+	snprintf(cmd, sizeof(cmd), "cat /proc/%s", file);
+
+	if (ssh_channel_request_exec(channel, cmd) != SSH_OK) goto error;
+	
+	buf = malloc(capacity + 1);
+	if (!buf) goto error;
+		
+	
+	while ((n = ssh_channel_read(channel, buf + size, capacity - size, 0)) > 0) {
+		size += n;
+		if (size == capacity) {
+			capacity *= 2;
+			char *tmp = realloc(buf, capacity +1);
+			if (!tmp) goto error;
+			buf = tmp;
+		}
+	}
+ 
+	if (n < 0) goto error;
+ 	
+	buf[size] = '\0';
+	*buffer = buf;
+
+	ssh_channel_close(channel);
+	ssh_channel_free(channel);
+ 
+	return SSH_OK;
+
+error:
+	free(buf);
+	if (channel) {
+		ssh_channel_close(channel);
+		ssh_channel_free(channel);
+	}
+	return SSH_ERROR;
 }
