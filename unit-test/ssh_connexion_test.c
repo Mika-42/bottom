9ecd45f6d55d3765c3810a14864c98248bf19f99
potdfleure <franjolauh@gmail.com>
2025-12-25 15:54:32 +0100

    adding the beginning of the unit test for ssh_connexion

---
+++ unit-test/ssh_connexion_test.c
@@ -0,0 +1,15 @@
+#include <libssh/libssh.h>
+#include <stdio.h>
+
+#include "ssh_connexion.h"
+
+int print_intro() {
+	char *presentation = "Pour faire ce teste unitaire, il vous faut openssh d'installé sur votre machine, si ce n'est pas le cas: \n";
+	printf();
+}
+
+int main() {
+	
+
+	return 0;
+}
