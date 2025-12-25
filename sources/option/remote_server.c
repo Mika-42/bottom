ea58f63f62017e1e834e3e4bd469d88a19d91212
ieto <ietob8@gmail.com>
2025-12-25 17:57:20 +0100

    convention remote server

--- sources/option/remote_server.c
+++ sources/option/remote_server.c
@@ -4,12 +4,16 @@
 
 error_code_t srv_str_duplicate(char *dest, char *src) {
 
-	if(!dest || !src) return NULLPTR_PARAMETER_ERROR;
+	if (!dest || !src) {
+		return NULLPTR_PARAMETER_ERROR;
+	}
 
     size_t len = strnlen(src, field_size);
 
-	if(len >= field_size) return BUFFER_TOO_LONG;
-	
+	if (len >= field_size) {
+		return BUFFER_TOO_LONG;
+	}
+
 	memcpy(dest, src, len);
     dest[len] = '\0';
 
@@ -18,12 +22,16 @@ error_code_t srv_str_duplicate(char *dest, char *src) {
 
 error_code_t srv_str_to_uint16(unsigned short *dest, char *src) {
 
-	if(!dest || !src) return NULLPTR_PARAMETER_ERROR;
+	if (!dest || !src) {
+		return NULLPTR_PARAMETER_ERROR;
+	}
 
 	char *endptr;
 	long n = strtol(src, &endptr, 10);
 
-	if(*endptr != '\0' || n < 0 || (size_t)n > max_port) return INVALID_ARGUMENT;
+	if (*endptr != '\0' || n < 0 || (size_t)n > max_port) {
+		return INVALID_ARGUMENT;
+	}
 	*dest = (unsigned short)n;
 
 	return SUCCESS;
@@ -31,8 +39,10 @@ error_code_t srv_str_to_uint16(unsigned short *dest, char *src) {
 
 error_code_t srv_str_to_connexion(connexion_type_t *dest, char *src) {
 	
-	if(!dest || !src) return NULLPTR_PARAMETER_ERROR;
-	
+	if (!dest || !src) {
+		return NULLPTR_PARAMETER_ERROR;
+	}
+
 	if (strcasecmp(src, "ssh") == 0) {
 		*dest = SSH;
 		return SUCCESS;
@@ -48,22 +58,28 @@ error_code_t srv_str_to_connexion(connexion_type_t *dest, char *src) {
 
 error_code_t srv_str_split_at(char *dest_lhs, char *dest_rhs, char *src) {
 
-	if(!dest_lhs || !dest_rhs || !src) return NULLPTR_PARAMETER_ERROR;
+	if (!dest_lhs || !dest_rhs || !src) {
+		return NULLPTR_PARAMETER_ERROR;
+	}
 
 	char copy[field_size];	
 	error_code_t err = srv_str_duplicate(copy, src);
-	if(err != SUCCESS) return err;
+	if (err != SUCCESS) {
+		return err;
+	}
 
 	char *separator = strchr(copy, '@');
-	if (!separator || separator == copy || *(separator + 1) == '\0')
+	if (!separator || separator == copy || *(separator + 1) == '\0') {
 		return INVALID_ARGUMENT;
-
-	if (strchr(separator + 1, '@'))
+	}
+	if (strchr(separator + 1, '@')) {
 		return INVALID_ARGUMENT;
-
+	}
 	err = srv_str_duplicate(dest_rhs, separator + 1);
-	if(err != SUCCESS) return err;
-	
+	if (err != SUCCESS) {
+		return err;
+	}
+
 	*separator = '\0';
 
 	return srv_str_duplicate(dest_lhs, copy);
