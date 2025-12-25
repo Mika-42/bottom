959554b4eebcce9b9d8e7e34d1e52b520f2667f0
ieto <ietob8@gmail.com>
2025-12-25 13:21:01 +0100

    convention ssh connexion

--- sources/network/serialization.h
+++ sources/network/serialization.h
@@ -4,7 +4,7 @@
 #include "processus_array.h" 
 
 int process_to_string(const processus_t *proc, char *buffer, size_t max_size);
-char* serialize_process_list(processus_array_t *array);
+char *serialize_process_list(processus_array_t *array);
 int send_data(int socket_fd, processus_array_t *array);
 
 #endif
