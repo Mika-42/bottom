4d99d428993c2ddea96d547491c8a48f7c63e6d4
ieto <ietob8@gmail.com>
2025-12-26 02:20:10 +0100

    ui utils convention

--- sources/user_interface/ui_utils.c
+++ sources/user_interface/ui_utils.c
@@ -3,7 +3,9 @@
 
 void ui_utils_clamp_int(int *value, const int min_value, const int max_value) {
 	
-	if(min_value > max_value) return;
+	if (min_value > max_value) {
+		return;
+	}
 
 	if (*value < min_value) {
 		*value = min_value;
@@ -14,8 +16,10 @@ void ui_utils_clamp_int(int *value, const int min_value, const int max_value) {
 
 void ui_utils_clamp_size_t(size_t *value, const size_t min_value, const size_t max_value) {
 
-	if(min_value > max_value) return;
-	
+	if (min_value > max_value) {
+		return;
+	}
+
 	if (*value < min_value) {
 		*value = min_value;
 	} else if (*value > max_value) {
