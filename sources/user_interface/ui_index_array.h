81152ffbfbf17d9fe0f42f36bdb4c1487bde4df1
ieto <ietob8@gmail.com>
2025-12-26 02:18:35 +0100

    ui page convention

--- sources/user_interface/ui_index_array.h
+++ sources/user_interface/ui_index_array.h
@@ -10,8 +10,8 @@ typedef struct index_array_t {
 	size_t capacity;
 } index_array_t;
 
-void	ui_index_array_reset(index_array_t *array);
-[[nodiscard]] size_t* ui_index_array_emplace_back(index_array_t *array, size_t value);
-void	ui_index_array_free(index_array_t *array);
+void ui_index_array_reset(index_array_t *array);
+[[nodiscard]] size_t *ui_index_array_emplace_back(index_array_t *array, size_t value);
+void ui_index_array_free(index_array_t *array);
 
 #endif /* UI_INDEX_ARRAY_H */
