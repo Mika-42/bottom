0a077f5f25b62bdb2bc8ca486634831faf5cf2ca
Matis <matis.mikael@gmail.com>
2025-12-31 02:35:00 +0100

    add [[nodiscard]] in stat_parser.h

--- sources/parser/stat_parser.h
+++ sources/parser/stat_parser.h
@@ -4,9 +4,9 @@
 #include "error.h"
 #include "processus.h"
 
-error_code_t stat_stat_parser(processus_t *proc, char *line);
-error_code_t stat_global_stat_parser(long *cpu_total, time_t *boot_time, char *line);
-error_code_t stat_user_parser(processus_t *proc, char *line);
-error_code_t stat_null_separated_parser(char *buffer, size_t size, char out[][PROC_CMD_LEN]);
+[[nodiscard]] error_code_t stat_stat_parser(processus_t *proc, char *line);
+[[nodiscard]] error_code_t stat_global_stat_parser(long *cpu_total, time_t *boot_time, char *line);
+[[nodiscard]] error_code_t stat_user_parser(processus_t *proc, char *line);
+[[nodiscard]] error_code_t stat_null_separated_parser(char *buffer, size_t size, char out[][PROC_CMD_LEN]);
 
 #endif /* STAT_PARSER_H */
