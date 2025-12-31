13234c26b16020721ad36268acef4ea647dfc408
Matis <matis.mikael@gmail.com>
2025-12-31 02:53:11 +0100

    clean code in processus_sort.h

--- sources/processus/processus_sort.h
+++ sources/processus/processus_sort.h
@@ -5,19 +5,19 @@
 #include <string.h>
 #include "processus_array.h"
 
-void	proc_array_sort(processus_array_t *array, proc_compare_t cmp);
-int	pid_asc(const processus_t *lhs, const processus_t *rhs);
-int	pid_dsc(const processus_t *lhs, const processus_t *rhs);
-int	state_asc(const processus_t *lhs, const processus_t *rhs);
-int	state_dsc(const processus_t *lhs, const processus_t *rhs);
-int	ram_asc(const processus_t *lhs, const processus_t *rhs);
-int	ram_dsc(const processus_t *lhs, const processus_t *rhs);
-int	name_asc(const processus_t *lhs, const processus_t *rhs);
-int	name_dsc(const processus_t *lhs, const processus_t *rhs);
-int	user_asc(const processus_t *lhs, const processus_t *rhs);
-int	user_dsc(const processus_t *lhs, const processus_t *rhs);
-int	time_asc(const processus_t *lhs, const processus_t *rhs);
-int	time_dsc(const processus_t *lhs, const processus_t *rhs);
-int	cpu_asc(const processus_t *lhs, const processus_t *rhs);
-int	cpu_dsc(const processus_t *lhs, const processus_t *rhs);
+void proc_array_sort(processus_array_t *array, proc_compare_t cmp);
+[[nodiscard]] int pid_asc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int pid_dsc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int state_asc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int state_dsc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int ram_asc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int ram_dsc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int name_asc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int name_dsc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int user_asc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int user_dsc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int time_asc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int time_dsc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int cpu_asc(const processus_t *lhs, const processus_t *rhs);
+[[nodiscard]] int cpu_dsc(const processus_t *lhs, const processus_t *rhs);
 #endif /* PROCESSUS_SORT_H */
