fed93bdce35ac28e6895c82f0047320fd6f9ebd3
potdfleure <franjolauh@gmail.com>
2026-01-01 16:21:58 +0100

    doxygen comment command_option.h

--- sources/command_option.h
+++ sources/command_option.h
@@ -12,6 +12,17 @@ typedef struct flag_t {
     bool dry_run : 1;
 } flag_t;
 
+/**
+ * @brief Récupère les option données avec le lancement du programme pour les gérer
+ *
+ * @param argc Nombre d'argument
+ * @param argv Tableau des différents arguments donnés
+ * @param flag Structure dans laquel est stocké différent flag permettant de savoir quoi lancer plus tard
+ * @param sessions Sessions SSH des différentes machines connectées au programme
+ * @param cfg_file Strucure qui va contenir les inforamtions de connexion de toutes les machines distantes récupérées depuis le fichier de configuration
+ *
+ * @return Un code d'erreur défini dans error.h
+ */
 [[nodiscard]] error_code_t command_run(int argc, char *argv[], flag_t *flag, ssh_session_array_t *sessions, config_file_t *cfg_file);
 
 #endif 
