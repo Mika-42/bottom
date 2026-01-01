2f4d51608446967b5666129aea8678fe5345a11a
potdfleure <franjolauh@gmail.com>
2026-01-01 17:17:54 +0100

    doxygen comment ssh_processus.h

--- sources/network/ssh_processus.h
+++ sources/network/ssh_processus.h
@@ -7,6 +7,14 @@
 #include "ssh_connexion.h"
 #include "processus_array.h"
 
+/**
+ * @brief Met à jour les information des processus en connexion SSH
+ *
+ * @param array Tableau de processus à mettre à jour
+ * @param session Sessioin utilisé pour la communication
+ *
+ * @return Un code d'erreur défini dans error.h
+ */
 [[nodiscard]] error_code_t ssh_array_update(processus_array_t *array, ssh_session session);
 
 #endif //SSH_PROCESSUS_H
