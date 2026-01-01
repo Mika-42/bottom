f5b55fe51bf2ebd80f7bc0e54b094c16a43dbaae
potdfleure <franjolauh@gmail.com>
2026-01-01 17:01:07 +0100

    doxygen comment thread.h

--- sources/thread.h
+++ sources/thread.h
@@ -13,18 +13,38 @@ typedef struct double_buffer_t {
     atomic_int active;
 } double_buffer_t;
 
+/**
+ * @brief Structure contenant les arguments d'un thread
+ */
 typedef struct thread_args_t {
     //unique
-    user_selection_t *selection;
+    user_selection_t *selection;	/**< Selection qui définie plusieurs chose propre à chaque machine */
 
     //shared
-    double_buffer_t *array;
-    ssh_session session;
-    char machine_name[32];
+    double_buffer_t *array;		/**< Un double buffer */
+    ssh_session session;		/**< Session SSH de la machine */
+    char machine_name[32];		/**< Nom de la machine */
 } thread_args_t;
 
+/**
+ * @brief Met à jour en continue une liste de processus en local ou en connexion SSH
+ * 
+ * @param arg Pointeur générique pointant vers une structure thread_args_t
+ */
 void *proc_task(void *arg);
+
+/**
+ * @brief Met à jour en continue l'affichage des processus et de lire le clavier
+ *
+ * @param arg Pointeur générique pointant vers une structure thread_args_t
+ */
 void *ui_task(void *arg);
+
+/**
+ * @brief Permet d'agir en continue sur un processus en local ou en connexion SSH à partir d'une selection utilisateur
+ *
+ * @param arg Pointeur générique pointant vers une structure thread_args_t
+ */
 void *manager_task(void *arg);
 
 #endif /* THREAD_H */
